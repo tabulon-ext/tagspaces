fdea9ade3e49131b95934320c0a4bc22e4f9f40e
Mladen Sapundshiev <mladen.sapundshiev@structmedia.com>
2018-07-03 15:59:33 +0200

    TSN430-appOnboardingNew

--- app/components/AppOnboarding.js
+++ app/components/AppOnboarding.js
@@ -23,16 +23,22 @@ import i18n from '../services/i18n';
 
 class AppOnboarding extends Component {
   state = {
-    run: false
+    run: false,
+    continuous: false,
+    showProgress: false,
+    showSkipButton: false,
   };
 
-  /* componentDidMount() {
+  componentDidMount() {
     setTimeout(() => {
       this.setState({
-        run: true
+        run: true,
+        continuous: true,
+        showProgress: true,
+        showSkipButton: true,
       });
     }, 1000);
-  } */
+  }
 
   callback = (data) => {
     const { action, index, type } = data;
@@ -40,23 +46,80 @@ class AppOnboarding extends Component {
 
   render() {
     const { run } = this.state;
+    const { continuous } = this.state;
+    const { showProgress } = this.state;
+    const { showSkipButton } = this.state;
     const steps = [
       {
-        target: '[data-tid=createNewLocation]',
+        title: i18n.t('welcomeTitle'),
+        target: '[data-tid=welcomePanel]',
         content: i18n.t('welcomeContent'),
+        placement: 'center',
+      },
+      {
+        title: i18n.t('connectingLocationTitle'),
+        target: '[data-tid=createNewLocation]',
+        content: i18n.t('createNewLocationContent'),
         placement: 'bottom',
       },
       {
-        target: '[data-tid=aboutTagSpaces]',
-        content: 'Hello world',
+        title: i18n.t('folderNavigatorTitle'),
+        target: '[data-tid=locationList]',
+        content: i18n.t('showLocationsContent'),
         placement: 'bottom',
       },
+      {
+        title: i18n.t('taglibraryTitle'),
+        target: '[data-tid=tagLibrary]',
+        content: i18n.t('tagGroupsContent'),
+        placement: 'bottom',
+      },
+      {
+        title: i18n.t('searchTitle'),
+        target: '[data-tid=search]',
+        content: i18n.t('searchareaContent'),
+        placement: 'bottom',
+      },
+      {
+        title: i18n.t('perspectiveTitle'),
+        target: '[data-tid=perspectiveManager]',
+        content: i18n.t('perspectiveviewContent'),
+        placement: 'bottom',
+      },
+      {
+        title: i18n.t('settings'),
+        target: '[data-tid=settings]',
+        content: i18n.t('settingsdialogContent'),
+        placement: 'bottom',
+      },
+      {
+        title: i18n.t('aboutTitle'),
+        target: '[data-tid=aboutTagSpaces]',
+        content: i18n.t('usefulinformationContent'),
+        placement: 'bottom',
+      },
+      {
+        title: i18n.t('enjoyTitle'),
+        target: '[data-tid=welcomePanel]',
+        content: i18n.t('thankyouContent'),
+        placement: 'center',
+      },
     ];
+
     return (
       <Joyride
         steps={steps}
         run={run}
+        continuous={continuous}
+        showProgress={showProgress}
+        showSkipButton={showSkipButton}
         callback={this.callback}
+        styles={{
+          tooltipHeader: {
+            borderBottomColor: '#f04',
+          },
+        }
+        }
       />
     );
   }
